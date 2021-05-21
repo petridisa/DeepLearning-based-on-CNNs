c4e3a98d6fdce9373736444ddafdd2ef2059d283
Theodoros Giannilias <32840612+Teokiller16e@users.noreply.github.com>
2021-05-21 22:50:32 +0300

    Architectures/Main updated

--- architectures.py
+++ architectures.py
@@ -14,12 +14,8 @@ from tensorflow.python.keras.layers.normalization_v2 import BatchNormalization
 def createInitialModel():
     inputShape = (48, 48, 1)
     model = Sequential()
-    stride = 2
     # Mostly when we go deeper to the encoding part we want to increase the number of channels
     # Construction of ConvolutionLayer & MaxPooling set :
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=inputShape))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
     model.add(Conv2D(64, (3, 3), activation='relu', input_shape=inputShape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -28,6 +24,9 @@ def createInitialModel():
 
     model.add(Conv2D(256, (3, 3), activation='relu', input_shape=inputShape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    # model.add(Conv2D(256,(3,3),activation='relu',input_shape = inputShape))
+    # model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Flatten())
 
     model.add(Dense(256, activation='relu'))
@@ -43,31 +42,21 @@ def createAlternativeModel():
     model = Sequential()
 
     # Construction of ConvolutionLayer & MaxPooling set :
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=inputShape, strides=1))
-    model.add(Activation("relu"))
-    # model.add(MaxPooling2D(padding='same',pool_size=(2,2)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=inputShape))
+    model.add(MaxPooling2D(padding='same', pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3), padding='same', strides=1))
-    model.add(Activation("relu"))
-    # model.add(MaxPooling2D(padding='same',pool_size=(2,2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=inputShape))
+    model.add(MaxPooling2D(padding='same', pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation("relu"))
-    # model.add(UpSampling2D(size=(2,2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', input_shape=inputShape))
+    model.add(MaxPooling2D(padding='same', pool_size=(2, 2)))
 
-    model.add(Conv2D(128, (3, 3), padding='same'))
-    model.add(Activation("relu"))
-    # model.add(UpSampling2D(size=(2,2)))
-
-    model.add(Conv2D(256, (3, 3), padding='same'))
-    model.add(Activation("relu"))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', input_shape=inputShape))
+    model.add(MaxPooling2D(padding='same', pool_size=(2, 2)))
 
     model.add(Flatten())
-    model.add(Dense(256))
-    model.add(Activation("relu"))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7, activation='softmax'))
 
-    model.add(Dense(7))
-    model.add(Activation("softmax"))
-    print(model.summary())
     return model
-
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.python.ops.gen_batch_ops import batch
-from architectures import createInitialModel
+from architectures import createAlternativeModel, createInitialModel
 from sklearn.model_selection import train_test_split
 from IPython.display import clear_output
 import time
@@ -13,12 +13,13 @@ import time
 #Plot Loss
 def display_loss(history):
     plt.figure(figsize=[8,6])
-    plt.plot(history.history['loss'],'red',linewidth = 3.0)
+    plt.plot(history.history['loss'],'cyan',linewidth = 3.0)
     plt.plot(history.history['val_loss'],'black',ls='--',linewidth = 3.0)
     plt.legend(['Training Loss' , 'Validation Loss'],fontsize=18)
     plt.xlabel('Epochs' ,fontsize=16)
     plt.ylabel('Loss',fontsize=16)
     plt.title('Loss Curves',fontsize=16)
+    plt.savefig(experiments_path+"/Experiment"+number+"loss.png")
     plt.show()
 
 #Plot Accuracy:
@@ -30,34 +31,27 @@ def display_acc(history):
     plt.xlabel('Epochs' ,fontsize=16)
     plt.ylabel('Accuracy',fontsize=16)
     plt.title('Accuracy Curves',fontsize=16)
+    plt.savefig(experiments_path+"/Experiment"+str(number)+"acc.png")
     plt.show()
 
-
+number = input("Give number of experiment/index of architecture : \n")
 
 # change directory to load fer dataset from pandas
 cwd = os.getcwd()
-checkpoint_path = os.chdir('model_checkpoint')
+experiments_path = (cwd+'\\Experiments')
 cwd = (cwd+'/fer2013/fer2013')
-checkpoint_path = os.getcwd()
+#checkpoint_path = os.getcwd()
 
 #filename = os.path.join(os.path.curdir, 'fer2013', 'fer2013', 'fer2013.csv')
 basic_emotions = ["anger", "disgust", "fear", "happiness", "sadness", "surprise", "neutral"]
 
 df = pd.read_csv(cwd+"/fer2013.csv")
-
 images = []
-#emotions = df['emotion']
-#emotions = [basic_emotions[i] for i in df.emotion.values]
-#emotions = np.array(emotions)
-
 labels = df.emotion.values
-#labels = emotions
-#print(labels.shape)
-#print(emotions.shape)
+
 
 for i in range(df.shape[0]):
     image = df['pixels'][i].split()
-    #image = [float(i)/255 for i in image]
     image = np.reshape(image, (48, 48, 1))
     images.append(image)
 images = np.stack(images, axis=0)
@@ -65,50 +59,66 @@ img_array = np.array(images)
 
 
 # Split training and testing data
+# print("X_train shape : \n",x_train.shape),
+# print("X_test shape : \n",x_test.shape),
+# print("Y_train shape : \n",y_train.shape),
+# print("Y_test shape : \n",y_test.shape)
+
+
+# Convert to float 32 and  Normalizing dataset(Works-->Tested) :
+img_array = img_array.astype("float32")
+img_array = img_array / 255
+
+# Split train and test dataset :
 x_train, x_test, y_train, y_test = train_test_split(img_array, labels, random_state=0 ,test_size=0.1)
-#print("X_train shape : \n",x_train.shape),#print("X_test shape : \n",x_test.shape),#print("Y_train shape : \n",y_train.shape),#print("Y_test shape : \n",y_test.shape)
 
-# Convert to float 32
-x_train = x_train.astype("float32")
-x_test = x_test.astype("float32")
 
-# Normalizing train/test data(Works-->Tested) :
-x_train = x_train / 255
-x_test = x_test / 255
+
+#x_train = img_array[:28708]
+#x_test = img_array[28709:32299]
+#y_train = labels[:28708]
+#y_test = labels[28709:32299]
+
+
+# Initialize validation set :
+#x_val = img_array[32300:]
+#y_val = labels[32300:]
 
 # Initialize model from architectures file
+# model = createAlternativeModel()
 model = createInitialModel()
+#model.summary()
+with open(experiments_path+'/Experiment'+number+'.txt','w') as file:
+    model.summary(print_fn=lambda x: file.write(x+"\n"))
+
+
 
 #Modifying Optimizer's Learning Rate:
-opt = tf.optimizers.RMSprop(learning_rate=0.001)
+opt = tf.optimizers.RMSprop(learning_rate=0.0001)
 #opt = tensorflow.keras.optimizers.Adam(lr=0.01)
 model.compile(loss="sparse_categorical_crossentropy",optimizer=opt,metrics=['accuracy'])
 
-
-# Checkpoint:
-
-checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, monitor = 'val_acc',verbose=1, save_best_only=True)
-# fit model
-
+# loss="mean_squared_error",optimizer='Adam'
 
 # load obtained weights before overfitting & save specific model
 # classifier.load_weights('checkpoint_path')
 # classifier.save('shapes_cnn.h5')
 
 
-model_history = model.fit(x_train,y_train,batch_size=32,epochs=10,validation_split=0.2,callbacks=[checkpoint]) # overfitting on the 10th epoch meaning that it reaches the optimal weights
+model_history = model.fit(x_train,y_train,batch_size=32,epochs=10,validation_split=0.11)  # overfitting on the 10th epoch meaning that it reaches the optimal weights
 
 #finalModel.compile(loss="mean_squared_error",optimizer='Adam',metrics=['accuracy'])
 #finalModel.summary()
-
 # #Final Accuracy
-scores = model.evaluate(x_train,y_train,batch_size=32)
+scores = model.evaluate(x_test,y_test,batch_size=32)
 print("Model Accuracy : %.2f%%"%(scores[1]*100))
 
 display_acc(model_history)
 display_loss(model_history)
 
+
 # Here we test our model and in the range we specify how many images we want to predict
+"""
 for k in range(0,10):
     labeled = y_test[k]
     print(f' Actual label :',basic_emotions[labeled])
@@ -119,4 +129,4 @@ for k in range(0,10):
     time.sleep(5)
     clear_output(wait=True)
 
-
+"""
